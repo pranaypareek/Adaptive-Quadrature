8dc81db66afcab71ca60f2a78155112777316fbd
pranaypareek <pranay.metal@gmail.com>
2016-10-07 00:20:48 -0400

    creates task trees

--- AQstub.adb
+++ AQstub.adb
@@ -6,19 +6,49 @@ procedure AQstub is
   use int_io;
 
   index: integer;
-  procedure Rec(X: in integer) is
-  Y: integer;
+  tree_num: integer;
+  procedure Rec(X, tree_num: in integer) is
+    Y: integer;
+    N: integer;
+    task t1;
+    task t2;
+
+    task body t1 is
+    begin
+      put("t1 tree number is: ");
+      put(tree_num);
+
+      N := tree_num + 1;
+      put("task t1 x val is: ");
+      put(X);
+      New_line;
+      Y := X - 1;
+      if Y > 0 then
+        Rec(Y, N);
+      end if;  
+    end t1;
+
+    task body t2 is
+    begin
+      put("t2 tree number is: ");
+      put(tree_num);
+
+      N := tree_num + 1;
+      put("task t2 x val is: ");
+      put(X);
+      New_line;
+      Y := X - 1;
+      if Y > 0 then
+        Rec(Y, N);
+      end if;  
+    end t2;
   begin
-    put("the value of X now is: ");
-    put(X); New_line;
-    Y := X - 1;
-    if Y > 0 then
-      Rec(Y);
-    end if;
+    Null;
   end Rec;
 
 begin
   index := 10;
-  put("Making Rec call"); New_line;
-  Rec(index);
+  tree_num := 0;
+  put("Making Rec call: "); New_line;
+  Rec(index, tree_num);
 end AQstub;
